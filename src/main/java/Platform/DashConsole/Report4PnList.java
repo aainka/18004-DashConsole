5638dd2fbc75abd912cc4cbf5496c122c59fb28f
aainka <aainka@gmail.com>
2018-12-14 19:22:50 +0900

    fun25

---
+++ src/main/java/Platform/DashConsole/Report4PnList.java
@@ -0,0 +1,129 @@
+package Platform.DashConsole;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import com.barolab.HmTR;
+import com.barolab.HttpPrintStream;
+import com.taskadapter.redmineapi.RedmineException;
+import com.taskadapter.redmineapi.bean.Issue;
+import com.taskadapter.redmineapi.bean.SavedQuery;
+
+import lombok.extern.java.Log;
+
+@Log
+public class Report4PnList extends MainDashBoard {
+
+	HttpPrintStream h;
+	List<OV_Issue> nlist;
+
+	// 이름순 소트 : 고객편의사항
+	// 상태별 소트
+	public void test() {
+		login();
+		List<SavedQuery> savedQueries;
+		try {
+
+			List<Issue> list = redmine.getIssueManager().getIssues("vepg-si-pr", 160);// proj, query
+			nlist = OV_Issue.toList(list);
+			log.info("load pr count=" + list.size());
+
+			Ascending ascending = new Ascending();
+			Collections.sort(nlist, ascending);
+
+		} catch (RedmineException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		File fp = new File("C:/tmp/test.html");
+		try {
+			HttpPrintStream h = new HttpPrintStream(fp);
+
+			int count = 1;
+			h.println("<table class='mytable' >");
+			for (OV_Issue issue : nlist) {
+
+				if (match(issue.getStatus(), "Resolved", "Closed", "Feedback")) {
+					continue;
+
+				}
+				if (contains(issue.getSubject(), "(현상대기)", "R140예비", "R140검토")) {
+					continue;
+				}
+				log.info("#" + (count));
+
+				log.info(issue.getSubject() + " ==> " + issue.getAssignee());
+
+				// h.println("<tr>");
+
+				HmTR tr = new HmTR();
+
+				String url = "http://redmine.ericssonlg.com/redmine/issues/" + issue.getId();
+
+				String ref = mkHref("<a href=%s %s >%d</a>", qt2(url), " target=" + qt1("_sub"),issue.getId());
+				tr.addTD().setWidth(50).setAligh("center").add(count);
+				tr.addTD().setWidth(120).setAligh("center").add(ref);
+				tr.addTD().setWidth(120).add(issue.getStatus());
+				tr.addTD().add(issue.getSubject());
+				tr.addTD().setWidth(150).add(issue.getAssignee());
+				tr.toHTML(h);
+				count++;
+			}
+			h.println("</table>");
+			h.close();
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+	}
+	public String mkHref(String msg, Object ... args ) {
+		String s = String.format(msg, args);
+		System.out.println(s);
+		return s;
+		
+	}
+
+	public String qt1(String msg) {
+		return "'" + msg + "'";
+	}
+
+	public String qt2(String msg) {
+		return "\"" + msg + "\"";
+	}
+
+	public boolean match(String key, String... args) {
+		for (String arg : args) {
+			if (key.equals(arg)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean contains(String key, String... args) {
+		for (String arg : args) {
+			if (key.indexOf(arg) >= 0) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		new Report4PnList().test();
+	}
+
+	private class Ascending implements Comparator<OV_Issue> {
+
+		@Override
+		public int compare(OV_Issue arg0, OV_Issue arg1) {
+			return arg0.getAssignee().compareTo(arg1.getAssignee());
+		}
+
+	}
+}
---
+++ src/main/java/Platform/DashConsole/Report4Spendtime.java
@@ -0,0 +1,111 @@
+package Platform.DashConsole;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.barolab.HmTR;
+import com.barolab.HttpPrintStream;
+import com.taskadapter.redmineapi.RedmineException;
+import com.taskadapter.redmineapi.TimeEntryManager;
+import com.taskadapter.redmineapi.bean.TimeEntry;
+import com.taskadapter.redmineapi.internal.ResultsWrapper;
+
+public class Report4Spendtime extends MainDashBoard {
+
+	HttpPrintStream h;
+
+	public void test() {
+
+		List<OV_TimeEntry> listLogtime = null;
+
+		login();
+		TimeEntryManager timeEntryManager = redmine.getTimeEntryManager();
+		List<TimeEntry> totalTimeEntry = new ArrayList<TimeEntry>();
+
+		try {
+			// List<TimeEntry> elements = timeEntryManager.getTimeEntries();
+			final Map<String, String> params = new HashMap<>();
+			int totalFound = 1;
+			for (int offset = 0; offset < totalFound; offset += 90) {
+				params.put("limit", "90");
+				params.put("offset", "" + offset);
+				params.put("spent_on", "<2018-12-01|2018-12-30>");
+				ResultsWrapper<TimeEntry> resultList = timeEntryManager.getTimeEntries(params);
+				System.out.println("* time.offset=" + offset);
+				// System.out.println(" time.result=" + resultList.getResultsNumber());
+				// System.out.println(" time.max=" + resultList.getTotalFoundOnServer());
+				System.out.println("  time.list=" + totalTimeEntry.size() + "/" + totalFound);
+				totalFound = resultList.getTotalFoundOnServer();
+				// OV_TimeEntry.transform(resultList.getResults()); // for test.
+				totalTimeEntry.addAll(resultList.getResults());
+				// totalFound = 200;
+			}
+			listLogtime = OV_TimeEntry.transform(totalTimeEntry);
+			System.out.println("LogTime process ending.");
+		} catch (RedmineException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// ********************************
+		// * REPORTING
+		// *******************************
+
+		Collections.sort(listLogtime, new Ascending2());
+
+		File fp = new File("C:/tmp/ReportSpendtime.html");
+		try {
+			HttpPrintStream h = new HttpPrintStream(fp);
+			int count = 1;
+			h.println("<table class='mytable' >");
+
+			for (OV_TimeEntry te : listLogtime) {
+
+				HmTR tr = new HmTR();
+
+				// String url = "http://redmine.ericssonlg.com/redmine/issues/" + issue.getId();
+
+				// String ref = mkHref("<a href=%s %s >%d</a>", qt2(url), " target=" +
+				// qt1("_sub"),issue.getId());
+				tr.addTD().setWidth(50).setAligh("center").add(count);
+				tr.addTD().add(te.getUserName());
+				tr.addTD().add(te.getWeeknum());
+				tr.addTD().add(te.getHours());
+				tr.addTD().add(te.getSpentOn());
+
+				tr.toHTML(h);
+				count++;
+			}
+			h.println("</table>");
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+	
+	}
+
+	private class Ascending2 implements Comparator<OV_TimeEntry> {
+
+		@Override
+		public int compare(OV_TimeEntry arg0, OV_TimeEntry arg1) {
+			int v = arg0.getWeeknum() - arg1.getWeeknum();
+			if ( v == 0 ) {
+				return arg0.getUserName().compareTo(arg1.getUserName());
+			}
+			return v;
+		}
+
+	}
+	
+	public static void main(String[] args) {
+		new Report4Spendtime().test();
+	}
+
+}
